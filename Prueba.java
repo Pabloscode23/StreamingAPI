ab3b78126551867a8cd5074838fdf726134cbbc1
Inassarm <149412560+Inassarm@users.noreply.github.com>
2024-10-02 21:39:27 -0600

    Hola :)

---
+++ Prueba.java
@@ -0,0 +1,7 @@
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("Les habla Pablo");
+        System.out.println("Hola aqui Fabiola");
+        System.out.println("Hola aqui Isabella");
+    }
+}
