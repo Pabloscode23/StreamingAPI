537dcf02b22096bbfbebfd8c0c412426ebbd1a02
Inassarm <149412560+Inassarm@users.noreply.github.com>
2024-10-16 22:12:12 -0600

    Add files via upload
    Clase de Suscripcion

--- src/classes/Suscripcion.java
+++ src/classes/Suscripcion.java
@@ -1,3 +1,8 @@
+/**
+ * La clase Suscripcion: suscripción de un usuario a un servicio
+ *
+ *
+ */
 public class Suscripcion {
     int codigo;
     int codigoUsuario;
@@ -7,6 +12,17 @@ public class Suscripcion {
     float precio;
     boolean activo;
 
+    /**
+     *
+     *
+     * @param codigo
+     * @param codigoUsuario
+     * @param codigoProveedor
+     * @param codigoPlan
+     * @param meses
+     * @param precio
+     * @param activo
+     */
     public Suscripcion(int codigo, int codigoUsuario, int codigoProveedor, int codigoPlan, int meses, float precio, boolean activo) {
         this.codigo = codigo;
         this.codigoUsuario = codigoUsuario;
@@ -17,62 +33,137 @@ public class Suscripcion {
         this.activo = activo;
     }
 
+    /**
+     *
+     *
+     * @return código de suscripción
+     */
     public int getCodigo() {
         return codigo;
     }
 
+    /**
+     *
+     *
+     * @param codigo
+     */
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
 
+    /**
+     *
+     *
+     * @return código de usuario.
+     */
     public int getCodigoUsuario() {
         return codigoUsuario;
     }
 
+    /**
+     *
+     *
+     * @param codigoUsuario código del usuario.
+     */
     public void setCodigoUsuario(int codigoUsuario) {
         this.codigoUsuario = codigoUsuario;
     }
 
+    /**
+     *
+     *
+     * @return código del proveedor.
+     */
     public int getCodigoProveedor() {
         return codigoProveedor;
     }
 
+    /**
+     *
+     *
+     * @param codigoProveedor código del proveedor.
+     */
     public void setCodigoProveedor(int codigoProveedor) {
         this.codigoProveedor = codigoProveedor;
     }
 
+    /**
+     *
+     *
+     * @return código del plan.
+     */
     public int getCodigoPlan() {
         return codigoPlan;
     }
 
+    /**
+     *
+     *
+     * @param codigoPlan El nuevo código del plan.
+     */
     public void setCodigoPlan(int codigoPlan) {
         this.codigoPlan = codigoPlan;
     }
 
+    /**
+     *
+     *
+     * @return duración de la suscripción .
+     */
     public int getMeses() {
         return meses;
     }
 
+    /**
+     *
+     *
+     * @param meses duración de la suscripción
+     */
     public void setMeses(int meses) {
         this.meses = meses;
     }
 
+    /**
+     *
+     *
+     * @return precio de la suscripción.
+     */
     public float getPrecio() {
         return precio;
     }
 
+    /**
+     *
+     *
+     * @param precio precio
+     */
     public void setPrecio(float precio) {
         this.precio = precio;
     }
 
+    /**
+     *
+     *
+     * @return suscripción está activa = true
+     */
     public boolean isActivo() {
         return activo;
     }
 
+    /**
+     *
+     *
+     * @param activo true si la suscripción debe ser activa
+     */
     public void setActivo(boolean activo) {
         this.activo = activo;
     }
 
+    /**
+     *
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return "Suscripcion{" +
@@ -86,4 +177,3 @@ public class Suscripcion {
                 '}';
     }
 }
-
